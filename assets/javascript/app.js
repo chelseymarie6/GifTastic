6016963b6bb3c921b4a636a07f01cb16abadea69
Chelsey Dolan <chelseymarie6@gmail.com>
2017-09-14 16:38:41 -0700

    85 perecent there

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,72 @@
-var topics = ["Star Trek", "Star Wars", "SNL", "Game of Thrones"];
+var movieArray = ["Star Trek", "Star Wars", "Jurassic Park", "The Godfather", "Speed", "The Dark Knight", "Pulp Fiction", "Lord of the Rings", "Fight Club", "Inception", "The Matrix", "Spirited Away", "Interstellar", "saving Private Ryan"];
+var buttonsHTML = " ";
+var newMovieValue;
+var searchMovie;
+var giphyArray = [];
 
-$("#gif-button").html("This is a fantastic gif generator button!");
 
-$("#gif-button").on("click", function() {
-    // var gif = $(this).attr("data-name");
-    var queryURL = "http://api.giphy.com/v1/gifs/trending?api_key=2a046d21cf8b42c98cb59a53cdb927f9";
+function generateButtons() {
+    for (var i = 0; i < movieArray.length; i++) {
+        buttonsHTML += "<button class='btn btn-lrg btn-primary movie-buttons' data-movie=" + movieArray[i] + ">" + movieArray[i] + "</button>";
+    }
+
+    $("#movie-buttons-container").html(buttonsHTML);
+}
+
+$(document).ready(function (){
+
+    generateButtons();
+
+$("body").on("click", "#add-movie", function(event) {
+    event.preventDefault();
+    newMovieValue = $("movie-input").val();
+    newButton = "<button class='btn btn-lrg btn-primary movie-buttons' data-movie=" + newMovieValue + ">" + newMovieValue + "</button>";
+    $("#movie-buttons-container").append(newButton);
+
+});
+
+$("body").on("click", ".movie-buttons", function(event) {
+    $(".giphy-div").empty();
+    searchMovie = $(this).attr("data-movie");
+    
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchMovie + "&limit=10" + "&api_key=2a046d21cf8b42c98cb59a53cdb927f9";
+
+    console.log(queryURL);
 
     $.ajax({
         url: queryURL,
         method: 'GET'
       }).done(function(response) {
-        console.log(response);
+        
+        console.log(response.data);
+
+        for (var i = 0; i < response.data.length; i++) {
+            console.log(response.data[i]);
+
+            $('.giphy-div').append("<div class='outer-container'><p class='title'>Rating: "+ response.data[i].rating.toUpperCase() +"</p><div class='image-container'><img class='images-returned img-responsive center-block'" + "data-still='" + response.data[i].images.downsized_still.url + "'" + "data-animate='" + response.data[i].images.downsized.url + "'" + "data-state='still'" + "src='" + response.data[i].images.downsized_still.url + "'></div></div>");
+
+            giphyArray.push(response.data[i].images.downsized.url);
+        }    
+
     });
 
 });
 
+$("body").on("click", ".images-returned", function(event) {
+    var state = $(this).attr("data-state");
+    var thisImgStill = $(this).attr("data-still");
+    var thisImgAnimate = $(this).attr("data-animate");
 
+    if (state === "still"){
+        $(this).attr("src", thisImgAnimate);
+        $(this).attr("data-state", "animate");
+    }
 
+    if (state !== "still"){
+        $(this).attr("src", thisImgStill);
+        $(this).attr("data-state", "still");
+    }
 
+});
 
-
-
-
+});
